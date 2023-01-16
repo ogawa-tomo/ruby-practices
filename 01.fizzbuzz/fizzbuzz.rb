3ff8d3221ee59613fa26c924abffb9f08b7fee56
tomo <o_tomo_1990@yahoo.co.jp>
2023-01-16 21:55:11 +0900

    不要な三項演算子を削除

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,6 +1,6 @@
 (1..20).each do |i|
-  multiple3 = i % 3 == 0 ? true : false
-  multiple5 = i % 5 == 0 ? true : false
+  multiple3 = i % 3 == 0
+  multiple5 = i % 5 == 0
   if multiple3 && multiple5
     puts 'FizzBuzz'
   elsif multiple3
