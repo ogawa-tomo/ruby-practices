3e0a169964ad126697887c70a426309cfd44a893
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-12 17:59:18 +0900

    各オプションを受け取る

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,7 +1,9 @@
+require 'optparse'
 
 def main
+  options = ARGV.getopts('l', 'c', 'w')
   files_data = get_files_data(ARGV)
-  output_files_info(files_data)
+  output_files_info(files_data, options)
 end
 
 def get_files_data(files_path)
@@ -17,24 +19,24 @@ def get_files_data(files_path)
   end
 end
 
-def output_files_info(files_data)
+def output_files_info(files_data, options)
   line_count_digit = get_total_line_count(files_data).to_s.length
   word_count_digit = get_total_word_count(files_data).to_s.length
   file_size_digit = get_total_file_size(files_data).to_s.length
 
   files_data.each do |file_data|
     puts [
-      file_data[:content].lines.length.to_s.rjust(line_count_digit),
-      file_data[:content].split.length.to_s.rjust(word_count_digit),
-      file_data[:content].bytesize.to_s.rjust(file_size_digit),
+      output_line_count?(options) ? file_data[:content].lines.length.to_s.rjust(line_count_digit) : '',
+      output_word_count?(options) ? file_data[:content].split.length.to_s.rjust(word_count_digit) : '',
+      output_file_size?(options) ? file_data[:content].bytesize.to_s.rjust(file_size_digit) : '',
       file_data[:name]
     ].join(' ')
   end
 
   puts [
-    get_total_line_count(files_data).to_s.rjust(line_count_digit),
-    get_total_word_count(files_data).to_s.rjust(word_count_digit),
-    get_total_file_size(files_data).to_s.rjust(file_size_digit),
+    output_line_count?(options) ? get_total_line_count(files_data).to_s.rjust(line_count_digit) : '',
+    output_word_count?(options) ? get_total_word_count(files_data).to_s.rjust(word_count_digit) : '',
+    output_file_size?(options) ? get_total_file_size(files_data).to_s.rjust(file_size_digit) : '',
     'total'
   ].join(' ') if files_data.length > 1
 end
@@ -49,6 +51,18 @@ end
 
 def get_total_file_size(files_data)
   files_data.map { |file_data| file_data[:content].bytesize }.sum
+end
+
+def output_line_count?(options)
+  !options['l'] && !options['w'] && !options['c'] ? true : options['l'] 
+end
+
+def output_word_count?(options)
+  !options['l'] && !options['w'] && !options['c'] ? true : options['w'] 
+end
+
+def output_file_size?(options)
+  !options['l'] && !options['w'] && !options['c'] ? true : options['c'] 
 end
 
 main
