4f5d2821fb5beb837c3aa44d825207e53eb399ac
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-12 16:41:28 +0900

    複数のファイルパスを受け取った場合に各ファイル情報を出力

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,6 +1,12 @@
 
 def main
-  File.open(ARGV[0], 'r') do |file|
+  ARGV.each do |file_path|
+    output_file_info(file_path)
+  end
+end
+
+def output_file_info(file_path)
+  File.open(file_path, 'r') do |file|
     content = file.read
     puts [
       content.lines.length,
