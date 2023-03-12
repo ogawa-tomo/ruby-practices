1d4af340eb9de98c0dcc7c2a68668724de74ee36
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-12 17:20:04 +0900

    出力の各行を揃える

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -14,18 +14,22 @@ def main
 end
 
 def output_files_info(files_data)
+  max_digit_line_count = files_data.map { |file_data| file_data[:content].lines.length }.sum.to_s.length
+  max_digit_word_count = files_data.map { |file_data| file_data[:content].split.length }.sum.to_s.length
+  max_digit_file_size = files_data.map { |file_data| file_data[:content].bytesize }.sum.to_s.length
+
   files_data.each do |file_data|
     puts [
-      file_data[:content].lines.length,
-      file_data[:content].split.length,
-      file_data[:content].bytesize,
+      file_data[:content].lines.length.to_s.rjust(max_digit_line_count),
+      file_data[:content].split.length.to_s.rjust(max_digit_word_count),
+      file_data[:content].bytesize.to_s.rjust(max_digit_file_size),
       file_data[:name]
     ].join(' ')
   end
   puts [
-    files_data.map { |file_data| file_data[:content].lines.length }.sum,
-    files_data.map { |file_data| file_data[:content].split.length }.sum,
-    files_data.map { |file_data| file_data[:content].bytesize }.sum,
+    files_data.map { |file_data| file_data[:content].lines.length }.sum.to_s.rjust(max_digit_line_count),
+    files_data.map { |file_data| file_data[:content].split.length }.sum.to_s.rjust(max_digit_word_count),
+    files_data.map { |file_data| file_data[:content].bytesize }.sum.to_s.rjust(max_digit_file_size),
     'total'
   ].join(' ') if files_data.length > 1
 end
