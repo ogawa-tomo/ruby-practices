776e3fe0506817f525b840e4020d97eb18f653c2
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-12 17:28:10 +0900

    リファクタ

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,6 +1,11 @@
 
 def main
-  files_data = ARGV.map do |file_path|
+  files_data = get_files_data(ARGV)
+  output_files_info(files_data)
+end
+
+def get_files_data(files_path)
+  files_path.map do |file_path|
     content = nil
     File.open(file_path) do |file|
       content = file.read
@@ -10,28 +15,40 @@ def main
       content: content
     }
   end
-  output_files_info(files_data)
 end
 
 def output_files_info(files_data)
-  max_digit_line_count = files_data.map { |file_data| file_data[:content].lines.length }.sum.to_s.length
-  max_digit_word_count = files_data.map { |file_data| file_data[:content].split.length }.sum.to_s.length
-  max_digit_file_size = files_data.map { |file_data| file_data[:content].bytesize }.sum.to_s.length
+  line_count_digit = get_total_line_count(files_data).to_s.length
+  word_count_digit = get_total_word_count(files_data).to_s.length
+  file_size_digit = get_total_file_size(files_data).to_s.length
 
   files_data.each do |file_data|
     puts [
-      file_data[:content].lines.length.to_s.rjust(max_digit_line_count),
-      file_data[:content].split.length.to_s.rjust(max_digit_word_count),
-      file_data[:content].bytesize.to_s.rjust(max_digit_file_size),
+      file_data[:content].lines.length.to_s.rjust(line_count_digit),
+      file_data[:content].split.length.to_s.rjust(word_count_digit),
+      file_data[:content].bytesize.to_s.rjust(file_size_digit),
       file_data[:name]
     ].join(' ')
   end
+
   puts [
-    files_data.map { |file_data| file_data[:content].lines.length }.sum.to_s.rjust(max_digit_line_count),
-    files_data.map { |file_data| file_data[:content].split.length }.sum.to_s.rjust(max_digit_word_count),
-    files_data.map { |file_data| file_data[:content].bytesize }.sum.to_s.rjust(max_digit_file_size),
+    get_total_line_count(files_data).to_s.rjust(line_count_digit),
+    get_total_word_count(files_data).to_s.rjust(word_count_digit),
+    get_total_file_size(files_data).to_s.rjust(file_size_digit),
     'total'
   ].join(' ') if files_data.length > 1
+end
+
+def get_total_line_count(files_data)
+  files_data.map { |file_data| file_data[:content].lines.length }.sum
+end
+
+def get_total_word_count(files_data)
+  files_data.map { |file_data| file_data[:content].split.length }.sum
+end
+
+def get_total_file_size(files_data)
+  files_data.map { |file_data| file_data[:content].bytesize }.sum
 end
 
 main
