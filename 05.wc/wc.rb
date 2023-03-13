699664dea611e3c1a55ecb2950af0d8412471802
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-13 22:01:23 +0900

    リファクタ

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -5,13 +5,38 @@ require 'optparse'
 
 def main
   options = ARGV.getopts('l', 'c', 'w')
+  output_data = get_output_data(options)
   if !ARGV.empty?
     files_data = get_files_data(ARGV)
-    output_files_info(files_data, options)
+    digit_data = get_digit_data(files_data)
+    output_each_files_info(files_data, output_data, digit_data)
+    return unless files_data.length > 1
+
+    output_total_files_info(files_data, output_data, digit_data)
   else
     content = $stdin.read
-    output_stdin_info(content, options)
+    output_stdin_info(content, output_data)
   end
+end
+
+def get_output_data(options)
+  {
+    line_count: output_line_count?(options),
+    word_count: output_word_count?(options),
+    byte_size: output_byte_size?(options)
+  }
+end
+
+def output_line_count?(options)
+  !options['l'] && !options['w'] && !options['c'] ? true : options['l']
+end
+
+def output_word_count?(options)
+  !options['l'] && !options['w'] && !options['c'] ? true : options['w']
+end
+
+def output_byte_size?(options)
+  !options['l'] && !options['w'] && !options['c'] ? true : options['c']
 end
 
 def get_files_data(files_path)
@@ -27,26 +52,30 @@ def get_files_data(files_path)
   end
 end
 
-def output_files_info(files_data, options)
-  line_count_digit = get_total_line_count(files_data).to_s.length
-  word_count_digit = get_total_word_count(files_data).to_s.length
-  byte_size_digit = get_total_byte_size(files_data).to_s.length
+def get_digit_data(files_data)
+  {
+    line_count_digit: get_total_line_count(files_data).to_s.length,
+    word_count_digit: get_total_word_count(files_data).to_s.length,
+    byte_size_digit: get_total_byte_size(files_data).to_s.length
+  }
+end
 
+def output_each_files_info(files_data, output_data, digit_data)
   files_data.each do |file_data|
     print file_data.length > 1 ? '  ' : ' '
-    print "#{file_data[:content].lines.length.to_s.rjust(line_count_digit)} " if output_line_count?(options)
-    print "#{file_data[:content].split.length.to_s.rjust(word_count_digit + 1)} " if output_word_count?(options)
-    print "#{file_data[:content].bytesize.to_s.rjust(byte_size_digit)} " if output_byte_size?(options)
+    print "#{file_data[:content].lines.length.to_s.rjust(digit_data[:line_count_digit])} " if output_data[:line_count]
+    print "#{file_data[:content].split.length.to_s.rjust(digit_data[:word_count_digit] + 1)} " if output_data[:word_count]
+    print "#{file_data[:content].bytesize.to_s.rjust(digit_data[:byte_size_digit])} " if output_data[:byte_size]
     print file_data[:name]
     puts ''
   end
+end
 
-  return unless files_data.length > 1
-
+def output_total_files_info(files_data, output_data, digit_data)
   print '  '
-  print "#{get_total_line_count(files_data).to_s.rjust(line_count_digit)} " if output_line_count?(options)
-  print "#{get_total_word_count(files_data).to_s.rjust(word_count_digit + 1)} " if output_word_count?(options)
-  print "#{get_total_byte_size(files_data).to_s.rjust(byte_size_digit)} " if output_byte_size?(options)
+  print "#{get_total_line_count(files_data).to_s.rjust(digit_data[:line_count_digit])} " if output_data[:line_count]
+  print "#{get_total_word_count(files_data).to_s.rjust(digit_data[:word_count_digit] + 1)} " if output_data[:word_count]
+  print "#{get_total_byte_size(files_data).to_s.rjust(digit_data[:byte_size_digit])} " if output_data[:byte_size]
   puts 'total'
 end
 
@@ -62,22 +91,10 @@ def get_total_byte_size(files_data)
   files_data.map { |file_data| file_data[:content].bytesize }.sum
 end
 
-def output_stdin_info(content, options)
-  print content.lines.length.to_s.rjust(7) if output_line_count?(options)
-  print content.split.length.to_s.rjust(8) if output_word_count?(options)
-  print content.bytesize.to_s.rjust(8) if output_byte_size?(options)
-end
-
-def output_line_count?(options)
-  !options['l'] && !options['w'] && !options['c'] ? true : options['l']
-end
-
-def output_word_count?(options)
-  !options['l'] && !options['w'] && !options['c'] ? true : options['w']
-end
-
-def output_byte_size?(options)
-  !options['l'] && !options['w'] && !options['c'] ? true : options['c']
+def output_stdin_info(content, output_data)
+  print content.lines.length.to_s.rjust(7) if output_data[:line_count]
+  print content.split.length.to_s.rjust(8) if output_data[:word_count]
+  print content.bytesize.to_s.rjust(8) if output_data[:byte_size]
 end
 
 main
