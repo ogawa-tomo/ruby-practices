fa919880f6d31c4276c0c797e514e8cdffcb3229
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-25 11:56:09 +0900

    mainの処理をフレームを作る処理とスコア計算の処理に分離

--- 06.bowling_object/main.rb
+++ 06.bowling_object/main.rb
@@ -4,30 +4,41 @@
 require_relative 'shot'
 require_relative 'frame'
 
-data = ARGV[0].split(',')
+def main
+  data = ARGV[0].split(',')
+  frames = make_frames(data)
+  game_score = calc_score(frames)
+  puts game_score
+end
 
-frame_num = 1
-pointer = 0
-frames = []
-loop do
-  break if data[pointer].nil?
+def make_frames(data)
+  frame_num = 1
+  pointer = 0
+  frames = []
+  loop do
+    break if data[pointer].nil?
 
-  shot = Shot.new(data[pointer])
+    shot = Shot.new(data[pointer])
 
-  if shot.strike?
-    frames << Frame.new(shot, Shot.new(0), frame_num)
-    pointer += 1
-  else
-    second_shot = Shot.new(data[pointer + 1])
-    frames << Frame.new(shot, second_shot, frame_num)
-    pointer += 2
+    if shot.strike?
+      frames << Frame.new(shot, Shot.new(0), frame_num)
+      pointer += 1
+    else
+      second_shot = Shot.new(data[pointer + 1])
+      frames << Frame.new(shot, second_shot, frame_num)
+      pointer += 2
+    end
+    frame_num += 1
   end
-  frame_num += 1
+  return frames
 end
 
-score = 0
-frames.each_with_index do |frame, i|
-  score += frame.get_score(frames[i + 1], frames[i + 2])
+def calc_score(frames)
+  score = 0
+  frames.each_with_index do |frame, i|
+    score += frame.get_score(frames[i + 1], frames[i + 2])
+  end
+  return score
 end
 
-puts score
+main
