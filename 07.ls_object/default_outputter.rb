bc4b7eb0e453d65ca1f09b20672fc0e8caf9191c
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-27 21:58:09 +0900

    column_widthとrows_numをインライン化

--- 07.ls_object/default_outputter.rb
+++ 07.ls_object/default_outputter.rb
@@ -5,16 +5,10 @@ require_relative 'outputter_base'
 class DefaultOutputter < OutputterBase
   COLUMN_NUM = 3
 
-  def initialize(all, reverse)
-    super(all, reverse)
-    @rows_num = get_rows_num
-    @column_width = get_column_width
-  end
-
   def output
-    (1..@rows_num).each do |r|
+    (1..rows_num).each do |r|
       (1..COLUMN_NUM).each do |c|
-        file_idx = (@rows_num * (c - 1)) + r - 1
+        file_idx = (rows_num * (c - 1)) + r - 1
         next if file_idx >= @files.length
 
         print_file(@files[file_idx])
@@ -25,16 +19,16 @@ class DefaultOutputter < OutputterBase
 
   private
 
-  def get_column_width
+  def column_width
     @files.max_by(&:length).length + 1
   end
 
   def print_file(file)
-    space_num = @column_width - file.length
+    space_num = column_width - file.length
     print file + ' ' * space_num
   end
 
-  def get_rows_num
+  def rows_num
     @files.length.fdiv(COLUMN_NUM).ceil
   end
 end
