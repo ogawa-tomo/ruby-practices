3ca49b2ebdf6b9fcc298c3f5eba266226b2894aa
tomo <o_tomo_1990@yahoo.co.jp>
2023-03-27 21:12:05 +0900

    通常のアウトプットクラスを分離

---
+++ 07.ls_object/default_outputter.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class DefaultOutputter
+  COLUMN_NUM = 3
+
+  def initialize(files)
+    @files = files
+    @rows_num = get_rows_num
+    @column_width = get_column_width
+  end
+
+  def output
+    (1..@rows_num).each do |r|
+      (1..COLUMN_NUM).each do |c|
+        file_idx = (@rows_num * (c - 1)) + r - 1
+        next if file_idx >= @files.length
+
+        print_file(@files[file_idx])
+      end
+      puts ''
+    end
+  end
+
+  private
+
+  def get_column_width
+    @files.max_by(&:length).length + 1
+  end
+
+  def print_file(file)
+    space_num = @column_width - file.length
+    print file + ' ' * space_num
+  end
+
+  def get_rows_num
+    @files.length.fdiv(COLUMN_NUM).ceil
+  end
+end
--- 07.ls_object/main.rb
+++ 07.ls_object/main.rb
@@ -3,52 +3,18 @@
 
 require 'optparse'
 require 'etc'
-
-COLUMN_NUM = 3
+require_relative 'default_outputter'
 
 def main
-  opt = OptionParser.new
-  params = {}
-  opt.on('-a') { |v| params[:a] = v }
-  opt.on('-r') { |v| params[:r] = v }
-  opt.on('-l') { |v| params[:l] = v }
-  opt.parse!(ARGV)
-
-  files = get_files(params[:a], params[:r])
-  params[:l] ? output_long(files) : output_default(files)
+  options = ARGV.getopts('a', 'r', 'l')
+  files = get_files(options['a'], options['r'])
+  options['l'] ? output_long(files) : DefaultOutputter.new(files).output
 end
 
 def get_files(all, reverse)
   files = all ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
   files.reverse! if reverse
   files
-end
-
-def output_default(files)
-  rows_num = get_rows_num(files)
-  column_width = get_column_width(files)
-  (1..rows_num).each do |r|
-    (1..COLUMN_NUM).each do |c|
-      file_idx = (rows_num * (c - 1)) + r - 1
-      next if file_idx >= files.length
-
-      print_file(files[file_idx], column_width)
-    end
-    puts ''
-  end
-end
-
-def get_column_width(files)
-  files.max_by(&:length).length + 1
-end
-
-def print_file(file, column_width)
-  space_num = column_width - file.length
-  print file + ' ' * space_num
-end
-
-def get_rows_num(files)
-  files.length.fdiv(COLUMN_NUM).ceil
 end
 
 def output_long(files)
